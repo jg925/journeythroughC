8fa18ea87f6d58f99159343158f2481fc109d79d
Jinny Gui <jinnygui@gmail.com>
2018-12-18 10:38:05 -0500

    initials ascii art

---
+++ C++/initials.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+//initals: JG
+
+
+int main(){
+  std::cout << "    J   GGG\n";
+  std::cout << "    J  G   G\n";
+  std::cout << "    J  G\n";
+  std::cout << "    J  G GGG\n";
+  std::cout << "J   J  G   G\n";
+  std::cout << "J   J  G   G\n";
+  std::cout << "JJJJJ   GGG\n";
+
+}
