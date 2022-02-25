1478604b9b9126888a845e1418d1b0ede99644db
jg925 <jg925@cornell.edu>
2022-02-25 00:39:23 -0500

    progress

---
+++ C++/linkedIn-LearningC++/5-3.cpp
@@ -0,0 +1,21 @@
+// Learning C++
+// Exercise 05_03
+// Function Parameters, by Eduardo Corpeño
+
+#include <iostream>
+
+using namespace std;
+
+// Takes arguments by value
+int square(int x) { return x * x; }
+
+int main() {
+    int a = 9, b;
+
+    cout << "a = " << a << ", b = " << b << endl;
+    // swap
+    cout << "a = " << a << ", b = " << b << endl;
+    // swap
+    cout << "a = " << a << ", b = " << b << endl;
+    return (0);
+}
